3cccd7cfd113d578544a090382d335be296f2d5b
Miguel Lopez Valenciano <miguellova@gmail.com>
2014-11-25 23:29:06 -0600

    Create the Top Down and Platformer projects
    Add a couple of sample sprites to complete the top down tutorial.

---
+++ 2DTopDown/Assets/Follow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+	public Transform target;
+
+	void Update () {
+		Vector3 targetPosition = target.position;
+		targetPosition.z = this.transform.position.z;
+		this.transform.position = targetPosition;
+	}
+}
