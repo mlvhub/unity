5db1d7e7d0d7f0fa3d189c4f41a10b09998b7882
Miguel Lopez V <miguel@firstfactory.com>
2015-02-08 15:27:36 -0600

    SpaceShooter progress

---
+++ UCSpaceShooter/Assets/CylinderController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CylinderController : MonoBehaviour {
+
+  public VirtualJoystick joystick;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    this.rigidbody2D.velocity = joystick.movement;
+    //this.rigidbody2D.AddForce(joystick.movement);
+	}
+}
